780553d35a75993db7304bbe7b1d7ecfbf9b4822
18EOR <42158152+18EOR@users.noreply.github.com>
2024-10-02 23:24:47 +0100

    added test auth route and middleware for user auth

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,12 +1,30 @@
 import { ObjectId } from 'mongodb';
 
-interface User {
+export type User = {
     _id?: ObjectId;
     username: string;
     email: string;
     password: string;
     registration_date: number;
     isSeller: boolean;
-}
+};
 
-export default User;
+export type RequestUserData = {
+    email: string;
+    name: string;
+};
+
+export type UserData = {
+    '@odata.context': string;
+    businessPhones: string[];
+    displayName: string;
+    givenName: string;
+    id: string;
+    jobTitle: string;
+    mail: string;
+    mobilePhone: string | null;
+    officeLocation: string | null;
+    preferredLanguage: string;
+    surname: string;
+    userPrincipalName: string;
+};
